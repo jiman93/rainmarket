a2d2f091feb2673f637445b517efc79566dbd630
Zulhafiz <zulhafiz06@gmail.com>
2025-06-18 17:05:07 +0800

    config fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/jiman93/rainmarket/", // Replace with your repo name
+  base: "/rainmarket/",
   plugins: [react()],
 });
